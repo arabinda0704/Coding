c9288b990b76c2c57403d6d1cc5181d65255755f
arabinda0704 <7arabinda@gmail.com>
2025-02-03 22:30:43 +0530

    modifying file 3-2-25.py

--- Python/3-2-25.py
+++ Python/3-2-25.py
@@ -15,6 +15,14 @@ for i in range(10):
     for j in range(i):
         if j<=i:
             print("*", end="")
-    else:
-        print(" ")
-    print("\n")
+        else:
+            print(" ", end="")
+    print()
+
+for i in range(1,5):
+    for j in range(1,8):
+        if j>=4-i+1 and j<=4+i-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
