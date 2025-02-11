6a71bb2c755a68d0c3d8591105566a38b989cff9
arabinda0704 <7arabinda@gmail.com>
2025-02-11 17:22:56 +0530

    adding final file

--- UCF 5.0/C/Intro_to_C.c
+++ UCF 5.0/C/Intro_to_C.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello World from Arabinda");
+    // printf("\033[13;40H");  // Moves the cursor to column 40, row 13
+    // printf("Hello World from Arabinda");
+    // int x;
+    // printf("Enter a number:");
+    //Unary Operator
+    int x,y,p,q;
+    int a=5;
+    int b=5;
+    int c=5;
+    int d=5;
+    x=++a;
+    y=b++;
+    p=--c;
+    q=d--;
+    printf("\n%d\n%d\n%d\n%d\n",x,y,p,q);
+
+    int z= sizeof(int);
+    printf("%d",z);
+    
+
+
 }
