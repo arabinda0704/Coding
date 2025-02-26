07c0392d63e5dafc9d325fbfacd91ac6c4ba725f
arabinda0704 <7arabinda@gmail.com>
2025-02-26 15:34:48 +0530

    modifying file

---
+++ Python/C-Schafar.py
@@ -0,0 +1,26 @@
+
+# STRINGS
+
+# message="Hello world"
+# message=message.replace("world","Universe")
+# print(message)
+
+# greeting='Hello'
+# name='Michael'
+
+# # message='{} {}, welcome!'.format(greeting,name)
+# message=f'{greeting} {name.upper()}, welcome!'
+
+# print(message)
+# print(help(str))
+
+
+# NUMBERS
+
+num=3.449
+print(type(num))
+print(abs(num))
+print(round(num,1))
+num_1="100"
+num_2="200"
+print(int(num_1)+int(num_2))
