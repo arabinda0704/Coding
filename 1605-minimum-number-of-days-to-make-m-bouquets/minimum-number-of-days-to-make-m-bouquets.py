a976fd9a02d2b16443cc5cb9a447ceceb85df71f
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-05-21 19:10:19 +0530

    Time: 455 ms (21.39%) | Memory: 31.6 MB (34.15%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,4 +1,17 @@
 class Solution:
+    def possible(self,arr, day, m, k):
+        n = len(arr)  # size of the array
+        cnt = 0
+        noOfB = 0
+        # count the number of bouquets
+        for i in range(n):
+            if arr[i] <= day:
+                cnt += 1
+            else:
+                noOfB += cnt // k
+                cnt = 0
+        noOfB += cnt // k
+        return noOfB >= m
     def minDays(self, arr: List[int], m: int, k: int) -> int:
         val = m * k
         n = len(arr)  # size of the array
@@ -17,17 +30,7 @@ class Solution:
         res=0
         while low<=high:
             mid=low+(high-low)//2
-            cnt = 0
-            noOfB = 0
-            # count the number of bouquets
-            for i in range(n):
-                if arr[i] <= mid:
-                    cnt += 1
-                else:
-                    noOfB += cnt // k
-                    cnt = 0
-            noOfB += cnt // k
-            if noOfB >= m:
+            if self.possible(arr, mid, m, k):
                 res=mid
                 high=mid-1
             else:
