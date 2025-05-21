5697fa41f42a6d8b94731439b95e9933e70c0d8c
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-05-21 19:21:31 +0530

    Time: 394 ms (32.54%) | Memory: 31.5 MB (74.08%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,36 +1,28 @@
-def possible(arr, day, m, k):
-    n = len(arr)  # size of the array
-    cnt = 0
-    noOfB = 0
-    # count the number of bouquets
-    for i in range(n):
-        if arr[i] <= day:
-            cnt += 1
-        else:
-            noOfB += cnt // k
-            cnt = 0
-    noOfB += cnt // k
-    return noOfB >= m
 class Solution:
     def minDays(self, arr: List[int], m: int, k: int) -> int:
         val = m * k
         n = len(arr)  # size of the array
         if val > n:
             return -1  # impossible case
-        # find maximum and minimum
-        mini = float('inf')
-        maxi = float('-inf')
-        for i in range(n):
-            mini = min(mini, arr[i])
-            maxi = max(maxi, arr[i])
+        
+        mini = min(arr)
+        maxi = max(arr)
 
-        # apply binary search
         low = mini
         high = maxi
         res=0
         while low<=high:
             mid=low+(high-low)//2
-            if possible(arr, mid, m, k):
+            cnt=0
+            noOfB=0
+            for i in range(n):
+                if arr[i]<=mid:
+                    cnt+=1
+                else:
+                    noOfB+=cnt//k
+                    cnt=0
+            noOfB+=cnt//k  
+            if noOfB>=m:
                 res=mid
                 high=mid-1
             else:
