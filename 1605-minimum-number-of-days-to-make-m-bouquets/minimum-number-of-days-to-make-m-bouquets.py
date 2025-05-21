35761ff9594e4c9f83bf5b56047d85baa0a8ab70
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-05-21 19:03:13 +0530

    Time: 435 ms (25.31%) | Memory: 31.6 MB (34.15%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minDays(self, arr: List[int], m: int, k: int) -> int:
+        val = m * k
+        n = len(arr)  # size of the array
+        if val > n:
+            return -1  # impossible case
+        # find maximum and minimum
+        mini = float('inf')
+        maxi = float('-inf')
+        for i in range(n):
+            mini = min(mini, arr[i])
+            maxi = max(maxi, arr[i])
+
+        # apply binary search
+        low = mini
+        high = maxi
+        res=0
+        while low<=high:
+            mid=low+(high-low)//2
+            cnt = 0
+            noOfB = 0
+            # count the number of bouquets
+            for i in range(n):
+                if arr[i] <= mid:
+                    cnt += 1
+                else:
+                    noOfB += cnt // k
+                    cnt = 0
+            noOfB += cnt // k
+            if noOfB >= m:
+                res=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return res
+
+
+        
