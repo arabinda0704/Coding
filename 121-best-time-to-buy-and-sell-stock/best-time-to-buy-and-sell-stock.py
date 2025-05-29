569969396818ab9dc0cb3e6357421f3126943409
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-05-29 20:13:33 +0530

    Time: 99 ms (47.53%) | Memory: 27.1 MB (8.97%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -5,14 +5,9 @@ class Solution:
         m_p=0
 
         while r<len(prices):
-            c_p=prices[r]-prices[l]
             if prices[l]<prices[r]:
-                m_p=max(c_p,m_p)
+                m_p=max(prices[r]-prices[l],m_p)
             else:
                 l=r
             r+=1
         return m_p
-#Internal working
-sol = Solution()
-# print(sol.maxProfit([7, 1, 5, 3, 6, 4]))
-print(Solution.maxProfit(sol,[7, 1, 5, 3, 6, 4]))
