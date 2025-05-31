e84b1bc059af82b2488e8f7518e91ba7ca9cdbb0
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-05-31 21:08:18 +0530

    Time: 8 ms (26.80%) | Memory: 17.9 MB (79.87%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,12 +1,26 @@
 class Solution:
+    # def isIsomorphic(self, s: str, t: str) -> bool:
+    #     def helper(s:str,t:str):
+    #         dict={}
+    #         if len(s)!=len(t):
+    #             return False
+    #         for i in range(len(s)):
+    #             if (s[i] in dict) and (dict[s[i]]!=t[i]):
+    #                 return False
+    #             dict[s[i]]=t[i]
+    #         return True        
+    #     return helper(s,t) and helper(t,s)
+
+    #Alternate Approach
     def isIsomorphic(self, s: str, t: str) -> bool:
-        def helper(s:str,t:str):
-            dict={}
-            if len(s)!=len(t):
+        mapST, mapTS = {}, {}
+
+        for i in range(len(s)):
+            c1, c2 = s[i], t[i]
+            if ((c1 in mapST and mapST[c1] != c2) or
+                (c2 in mapTS and mapTS[c2] != c1)):
                 return False
-            for i in range(len(s)):
-                if (s[i] in dict) and (dict[s[i]]!=t[i]):
-                    return False
-                dict[s[i]]=t[i]
-            return True        
-        return helper(s,t) and helper(t,s)
+            mapST[c1] = c2
+            mapTS[c2] = c1
+            
+        return True
