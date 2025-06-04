0d3f8e81c01f75e587a5c71248580bfcc941319c
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-04 19:50:39 +0530

    Time: 30 ms (37.52%) | Memory: 19.2 MB (86.04%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        #Extreme Brute Force
+        # Extreme Brute Force
         # res=[]
         # for i in range (len(nums)):
         #     nextGreater=-1
@@ -18,16 +18,37 @@ class Solution:
         #     res.append(nextGreater)
         # return res
 
-        #Better
-        res=[]
-        n=len(nums)
-        for i in range(n):
-            nextGreater=-1
-            for j in range (i+1,i+n):
-                ind=j%n   
-                if nums[ind] > nums[i]:
-                        nextGreater=nums[ind]
-                        break
-            res.append(nextGreater)
-        return res
+        # Better
+        # res=[]
+        # n=len(nums)
+        # for i in range(n):
+        #     nextGreater=-1
+        #     for j in range (i+1,i+n):
+        #         ind=j%n   
+        #         if nums[ind] > nums[i]:
+        #                 nextGreater=nums[ind]
+        #                 break
+        #     res.append(nextGreater)
+        # return res
+
         #Optimal
+        n = len(nums)
+        nge = [-1] * n
+        st = []
+
+
+        for i in range(2 * n - 1, -1, -1):
+            while st and st[-1] <= nums[i % n]:
+                st.pop()
+
+
+            if i < n:
+                if st:
+                    nge[i] = st[-1]
+            st.append(nums[i % n])
+        return nge
+
+
+
+
+
