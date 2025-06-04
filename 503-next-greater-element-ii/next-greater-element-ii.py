d650aab14d2414fc365dce33e366d4617d5c3913
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-04 18:33:00 +0530

    Time: 2034 ms (9.35%) | Memory: 19.1 MB (94.35%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,18 +1,33 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        #Extreme Brute Force
+        # res=[]
+        # for i in range (len(nums)):
+        #     nextGreater=-1
+        #     flag=True
+        #     for j in range(i+1,len(nums)):
+        #         if nums[j] > nums[i]:
+        #             flag=False
+        #             nextGreater=nums[j]
+        #             break
+        #     if nextGreater==-1 and flag:
+        #         for j in range(0,i):
+        #             if nums[j] > nums[i]:
+        #                 nextGreater=nums[j]   
+        #                 break
+        #     res.append(nextGreater)
+        # return res
+
+        #Better
         res=[]
-        for i in range (len(nums)):
+        n=len(nums)
+        for i in range(n):
             nextGreater=-1
-            flag=True
-            for j in range(i+1,len(nums)):
-                if nums[j] > nums[i]:
-                    flag=False
-                    nextGreater=nums[j]
-                    break
-            if nextGreater==-1 and flag:
-                for j in range(0,i):
-                    if nums[j] > nums[i]:
-                        nextGreater=nums[j]   
+            for j in range (i+1,i+n):
+                ind=j%n   
+                if nums[ind] > nums[i]:
+                        nextGreater=nums[ind]
                         break
             res.append(nextGreater)
         return res
+        #Optimal
