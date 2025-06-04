4742c2c4b3b031be309412dfaf2c3170f891edd5
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-04 14:46:33 +0530

    Time: 235 ms (4.16%) | Memory: 17.8 MB (80.76%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,35 @@
+class Solution:
+    def __init__(self):
+        self.q=deque()
+
+        
+
+    def Push(self, x: int) -> None:
+        # self.push(x)
+        self.q.append(x)
+
+        
+
+    def Pop(self) -> int:
+        for i in range(len(self.q)-1):
+            self.q.append(self.q.popleft())
+        return self.q.popleft()
+
+    def isValid(self, s: str) -> bool:
+        d=Solution()
+        for c in s:
+            if c == '(' or c == '{' or c == '[': 
+                d.Push(c)
+            else:
+                if len(d.q) == 0:
+                    return False
+
+                ch = d.Pop()
+                if (c == ')' and ch == '(') or (c == ']' and ch == '[') or (c == '}' and ch == '{'):
+                    continue
+                else:
+                    return False
+    
+        return len(d.q) == 0
+
+        
