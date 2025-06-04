dc98044adc0852e2b33083b2428e200e2dab6222
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-04 14:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (10.21%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,35 +1,51 @@
 class Solution:
-    def __init__(self):
-        self.q=deque()
+    # def __init__(self):
+    #     self.q=deque()
 
         
 
-    def Push(self, x: int) -> None:
-        # self.push(x)
-        self.q.append(x)
+    # def Push(self, x: int) -> None:
+    #     # self.push(x)
+    #     self.q.append(x)
 
         
 
-    def Pop(self) -> int:
-        for i in range(len(self.q)-1):
-            self.q.append(self.q.popleft())
-        return self.q.popleft()
+    # def Pop(self) -> int:
+    #     for i in range(len(self.q)-1):
+    #         self.q.append(self.q.popleft())
+    #     return self.q.popleft()
 
+    # def isValid(self, s: str) -> bool:
+    #     d=Solution()
+    #     for c in s:
+    #         if c in "({[": #c == '(' or c == '{' or c == '[': 
+    #             d.Push(c)
+    #         else:
+    #             if len(d.q) == 0:
+    #                 return False
+
+    #             ch = d.Pop()
+    #             if (c == ')' and ch == '(') or (c == ']' and ch == '[') or (c == '}' and ch == '{'):
+    #                 continue
+    #             else:
+    #                 return False
+    
+    #     return len(d.q) == 0
     def isValid(self, s: str) -> bool:
-        d=Solution()
+        d=[]
         for c in s:
             if c in "({[": #c == '(' or c == '{' or c == '[': 
-                d.Push(c)
+                d.append(c)
             else:
-                if len(d.q) == 0:
+                if len(d) == 0:
                     return False
 
-                ch = d.Pop()
+                ch = d.pop()
                 if (c == ')' and ch == '(') or (c == ']' and ch == '[') or (c == '}' and ch == '{'):
                     continue
                 else:
                     return False
     
-        return len(d.q) == 0
+        return len(d) == 0
 
         
