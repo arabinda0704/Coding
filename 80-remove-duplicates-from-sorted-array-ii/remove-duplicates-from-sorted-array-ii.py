f65ce358e0ad76e87aab4cf3b11356f6c5393821
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-06 22:27:12 +0530

    Time: 87 ms (46.64%) | Memory: 20.3 MB (80.19%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -1,12 +1,18 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if len(nums) <= 2:
-            return len(nums)
+        # if len(nums) <= 2:
+        #     return len(nums)
         
-        insert_pos = 2
-        for i in range(2, len(nums)):
-            # Compare current number with the element two positions before
-            if nums[i] != nums[insert_pos - 2]:
-                nums[insert_pos] = nums[i]
-                insert_pos += 1
-        return insert_pos
+        # insert_pos = 2
+        # for i in range(2, len(nums)):
+        #     # Compare current number with the element two positions before
+        #     if nums[i] != nums[insert_pos - 2]:
+        #         nums[insert_pos] = nums[i]
+        #         insert_pos += 1
+        # return insert_pos
+        l = 0
+        for num in nums:
+            if l < 2 or num != nums[l - 2]:
+                nums[l] = num
+                l += 1
+        return l
