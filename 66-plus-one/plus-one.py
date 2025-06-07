646e3b768caac9114177a6e75750337e4aff5d75
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-07 15:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (79.55%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,5 +1,22 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        # s=digits
+        # for i in range(len(s)-1,-1,-1):
+        #     if s[i]==9:
+        #         s[i] = 0
+        #     else:
+        #         s[i] += 1  
+        #         # return s #This line is not mandatory
+        #         break  
+        # # s=s[::-1]
+        # # if s[-1] == 0:
+        # #     s.append(1)
+
+        # # return s[::-1]
+        # if s[0]==0:   #As the question suggest that question will not contain any leading 0's
+        #     s.insert(0, 1)
+        # return s
+        #Another good approach
         s=digits
         for i in range(len(s)-1,-1,-1):
             if s[i]==9:
@@ -7,13 +24,5 @@ class Solution:
             else:
                 s[i] += 1  
                 return s
-                break  
-        # s=s[::-1]
-        # if s[-1] == 0:
-        #     s.append(1)
-
-        # return s[::-1]
-        if s[0]==0:   #As the question suggest that question will not contain any leading 0's
-            s.insert(0, 1)
+        s.insert(0, 1) #As the question suggest that question will not contain any leading 0's
         return s
-        
