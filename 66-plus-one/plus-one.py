a22df69df4b17388d328cd91c78ecb59c3a7ee84
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-07 14:55:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.77%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -2,12 +2,11 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         s=digits
         for i in range(len(s)-1,-1,-1):
-    
-            if s[i] != 9:
+            if s[i]==9:
+                s[i] = 0
+            else:
                 s[i] += 1  
                 break  
-            else:
-                s[i] = 0
         s=s[::-1]
         if s[-1] == 0:
             s.append(1)
