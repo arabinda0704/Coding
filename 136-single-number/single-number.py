679e628395419412d277ea6198cb17b6003d07b7
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-07 14:21:06 +0530

    Time: 13 ms (13.79%) | Memory: 19.7 MB (23.41%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # ans=0
+        # for i in nums:
+        #     ans^=i
+        # return ans
+        dict1 = defaultdict(int)  # default value for any key is 0
+
+        for i in range(len(nums)):
+            dict1[nums[i]] += 1
+        for i in range(len(nums)):
+            if dict1[nums[i]]==1:
+                return nums[i]
+                
