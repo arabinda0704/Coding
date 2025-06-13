a24cbfa3144caa2ab01042caacbeadeaf9b16227
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-13 23:00:26 +0530

    Time: 123 ms (38.17%) | Memory: 23 MB (83.32%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,21 +1,40 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        l=m=0
-        oddCount=0
-        res=0
-        for r in range(len(nums)):
-            if nums[r] & 1:
-                oddCount+=1
+        # l=m=0
+        # oddCount=0
+        # res=0
+        # for r in range(len(nums)):
+        #     if nums[r] & 1:
+        #         oddCount+=1
 
-            while oddCount>k:
-                if nums[l] & 1:
-                    oddCount-=1
-                l+=1   
-                m=l 
-            if oddCount==k:
-                while not (nums[m] & 1):
-                    m+=1
-                res+=(m-l+1)
+        #     while oddCount>k:
+        #         if nums[l] & 1:
+        #             oddCount-=1
+        #         l+=1   
+        #         m=l 
+        #     if oddCount==k:
+        #         while not (nums[m] & 1):
+        #             m+=1
+        #         res+=(m-l+1)
                    
-        return res
+        # return res
+
+        #One More Solution
+        n=[0]*len(nums)
+        for i in range(len(nums)):
+            if nums[i] & 1:
+                n[i]=1
+        def helper(x):
+            if x < 0:
+                return 0
+            res = l = cur = 0
+            for r in range(len(n)):
+                cur += n[r]
+                while cur > x:
+                    cur -= n[l]
+                    l += 1
+                res += (r - l + 1)
+            return res
+
+        return helper(k) - helper(k - 1)
                     
