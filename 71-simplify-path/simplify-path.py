7e2c3192392af6267e29fd0b8254699de68838cf
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-19 23:09:28 +0530

    Time: 1 ms (48.30%) | Memory: 17.8 MB (48.98%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,30 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        # st=[]
+        # cur=""
+        # for c in path+"/":
+        #     if c=="/":
+        #         if cur=="..":
+        #             if st:
+        #                 st.pop()
+        #         elif cur !="" and cur !=".":
+        #             st.append(cur)
+        #         cur=""
+        #     else:
+        #         cur+=c
+        # return "/"+"/".join(st)
+
+        #Another
+        st=[]
+        cur=""
+        for c in path+"/":
+            if c=="/":
+                if cur==".." and st:
+                    if st:
+                        st.pop()
+                elif cur !="" and cur !="." and cur != "..":
+                    st.append(cur)
+                cur=""
+            else:
+                cur+=c
+        return "/"+"/".join(st)
