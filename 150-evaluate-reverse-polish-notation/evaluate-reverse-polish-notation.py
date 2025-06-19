6c9b5d6cdb3d85afd245750e31e25714f98af5bf
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-06-19 20:37:41 +0530

    Time: 3 ms (65.05%) | Memory: 19.1 MB (88.07%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -17,7 +17,7 @@ class Solution:
     
             else:
                 st.append(int(tokens[i]))
-        return st.pop()
+        return st.pop() #st[0] or st[-1] or st.pop() are same here because st will contain exactly one element at the end
                 
 
         
