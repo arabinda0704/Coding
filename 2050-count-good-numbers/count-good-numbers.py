074b50b6087a600580176650d5a47cc4ab0c6ad9
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-07-21 16:51:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (42.55%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,12 +1,28 @@
 class Solution:
+    # def myPow(self, x: int, n: int, mod: int) -> int:
+    #     ans = 1
+    #     while n:
+    #         if n % 2:
+    #             ans = (ans * x) % mod
+    #         x = (x * x) % mod
+    #         n //= 2
+    #     return ans
+
+
+        #Using recurssion
     def myPow(self, x: int, n: int, mod: int) -> int:
-        ans = 1
-        while n:
-            if n % 2:
-                ans = (ans * x) % mod
-            x = (x * x) % mod
-            n //= 2
+        if n==0:
+            return 1
+        ans=self.myPow(x,n//2,mod)
+        ans*=ans
+        ans%=mod
+        if n%2:
+            ans*=x
+        ans%=mod
         return ans
+
+        
+
     def countGoodNumbers(self, n: int) -> int:
         mod = 10**9 + 7
         odd=n//2
