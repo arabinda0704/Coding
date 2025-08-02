d2cbb4eb7f8e606c641159c60b2de8d211d06ed1
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-08-02 19:00:56 +0530

    Time: 4 ms (32.45%) | Memory: 18.2 MB (21.34%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -13,18 +13,43 @@ class Solution:
         # return res
 
         #Better/Optimal
-        nums1Idx = {num : i for i, num in enumerate(nums1)}
-        res = [-1] * len(nums1)
+        # nums1Idx = {num : i for i, num in enumerate(nums1)}
+        # nums1Idx = {}  
 
-        stack = []
-        for i in range(len(nums2)):
-            cur = nums2[i]
-            while stack and cur > stack[-1]:
-                val = stack.pop()
-                idx = nums1Idx[val]
-                res[idx] = cur
-            if cur in nums1Idx:
-                stack.append(cur)
+        # for i, num in enumerate(nums1):
+        #     nums1Idx[num] = i 
+
+        # res = [-1] * len(nums1)
+
+        # stack = []
+        # for i in range(len(nums2)):
+        #     cur = nums2[i]
+        #     while stack and cur > stack[-1]:
+        #         val = stack.pop()
+        #         idx = nums1Idx[val]
+        #         res[idx] = cur
+        #     if cur in nums1Idx:
+        #         stack.append(cur)
+        # return res
+
+        #Another 
+        nums1Idx = {}  
+
+        for i, num in enumerate(nums1):
+            nums1Idx[num] = i
+        res = [-1] * len(nums1)
+        stack=[]
+        n=len(nums2)
+        for i in range(n-1,-1,-1):
+            while stack and stack[-1]<=nums2[i]:
+                stack.pop()
+
+            if nums2[i] in nums1Idx:
+                idx = nums1Idx[nums2[i]]
+                if stack:
+                    res[idx] = stack[-1]
+
+            stack.append(nums2[i])
         return res
 
 
