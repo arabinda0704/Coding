d2228123f59f91a375fc023e4319a9464f7c47dd
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-08-06 18:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (45.65%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -5,27 +5,42 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #Data Replacement
+        #Data Replacement  O(N) Space
+        # if not head or not head.next or not head.next.next:
+        #     return head
+        # l1=[]
+        # temp=head
+        # while temp and temp.next:
+        #     l1.append(temp.val)
+        #     if not temp.next:
+        #         break
+        #     temp=temp.next.next
+        # temp=head.next
+        # while temp:
+        #     l1.append(temp.val)
+        #     if not temp.next:
+        #         break
+        #     temp=temp.next.next
+        # i=0
+        # temp=head
+        # while temp and i<len(l1):
+        #     temp.val=l1[i]
+        #     i+=1
+        #     temp=temp.next
+        # return head
+
+        #Better O(1) Space
         if not head or not head.next or not head.next.next:
             return head
-        l1=[]
-        temp=head
-        while temp:
-            l1.append(temp.val)
-            if not temp.next:
-                break
-            temp=temp.next.next
-        temp=head.next
-        while temp:
-            l1.append(temp.val)
-            if not temp.next:
-                break
-            temp=temp.next.next
-        i=0
-        temp=head
-        while temp and i<len(l1):
-            temp.val=l1[i]
-            i+=1
-            temp=temp.next
+        odd,even=head,head.next
+        evenHead=head.next
+        while even and even.next:
+            odd.next=odd.next.next
+            even.next=even.next.next
+
+            odd=odd.next
+            even=even.next
+        odd.next=evenHead
         return head
+
 
