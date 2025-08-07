13c3794a613681afdb693b5e81654ec1f310cb72
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-08-07 14:59:50 +0530

    Time: 56 ms (13.12%) | Memory: 20.4 MB (5.57%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Brute fore extra space O(N)
+        seen = set()
+        cur = head
+        while cur:
+            if cur in seen:
+                return True
+            seen.add(cur)
+            cur = cur.next
+        return False
+        # Better O(1) Space
+        # slow,fast=head,head
+        # while fast and fast.next:
+        #     slow=slow.next
+        #     fast=fast.next.next
+        #     if slow==fast:
+        #         return True
+        # return False
+        
