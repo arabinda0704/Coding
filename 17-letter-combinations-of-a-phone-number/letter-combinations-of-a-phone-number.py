4dae3ddffd5e5c04de9aa1283748f6fd0001d631
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-08-08 16:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (69.02%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,9 +1,31 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
+        # if not digits:
+        #     return []
 
-        res = [""]
+        # res = [""]
+        # digitToChar = {
+        #     "2": "abc",
+        #     "3": "def",
+        #     "4": "ghi",
+        #     "5": "jkl",
+        #     "6": "mno",
+        #     "7": "qprs",
+        #     "8": "tuv",
+        #     "9": "wxyz",
+        # }
+
+        # for digit in digits:
+        #     tmp = []
+        #     for curStr in res:
+        #         for c in digitToChar[digit]:
+        #             tmp.append(curStr + c)
+        #     res = tmp
+        # return res
+
+        #Bacttrack
+
+        res = []
         digitToChar = {
             "2": "abc",
             "3": "def",
@@ -15,11 +37,15 @@ class Solution:
             "9": "wxyz",
         }
 
-        for digit in digits:
-            tmp = []
-            for curStr in res:
-                for c in digitToChar[digit]:
-                    tmp.append(curStr + c)
-            res = tmp
+        def backtrack(i, curStr):
+            if i == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+
         return res
         
