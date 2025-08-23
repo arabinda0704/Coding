53dfbc3eaa5a1a0bd94fdacd1f1c8cb4a2094f90
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-08-23 16:41:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (98.89%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.py
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.py
@@ -14,48 +14,48 @@ class Solution:
             curr=nxt
         return prev
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
-        curr =head
-        n=0
-        while curr:
-            curr=curr.next
-            n+=1
-
-        nge = [0] * n
-        st = []
-        nums=[]
-        temp=head
-        while temp:
-            nums.append(temp.val)
-            temp=temp.next
-        for i in range(n-1,-1,-1):
-            while st and st[-1] <= nums[i % n]:
-                st.pop()
-            if st:
-               nge[i]=st[-1]
-            
-            st.append(nums[i % n])
-        return nge
-
-        # with Better Space
+        # curr =head
         # n=0
+        # while curr:
+        #     curr=curr.next
+        #     n+=1
+
+        # nge = [0] * n
+        # st = []
+        # nums=[]
         # temp=head
         # while temp:
-        #     n+=1
+        #     nums.append(temp.val)
         #     temp=temp.next
-        # res=[0]*n
-        # last=self.reverse(head)
-        # i=0
-        # stack=[]
-        # while last:
-        #     while stack and last.val>=stack[-1]:
-        #         stack.pop()
-        #     if stack:
-        #         res[i]=stack[-1]
-        #     stack.append(last.val)
-        #     last=last.next
-        #     i+=1
-        # res.reverse()
-        # return res
+        # for i in range(n-1,-1,-1):
+        #     while st and st[-1] <= nums[i % n]:
+        #         st.pop()
+        #     if st:
+        #        nge[i]=st[-1]
+            
+        #     st.append(nums[i % n])
+        # return nge
+
+        # with Better Space
+        n=0
+        temp=head
+        while temp:
+            n+=1
+            temp=temp.next
+        res=[0]*n
+        last=self.reverse(head)
+        i=0
+        stack=[]
+        while last:
+            while stack and stack[-1]<=last.val:
+                stack.pop()
+            if stack:
+                res[i]=stack[-1]
+            stack.append(last.val)
+            last=last.next
+            i+=1
+        res.reverse()
+        return res
     
 
         
