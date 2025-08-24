7f33b1b3da1e7d1d1a3a91a14e5338d7b97921a7
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-08-24 18:00:21 +0530

    Time: 40 ms (61.66%) | Memory: 40.2 MB (32.31%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
@@ -5,18 +5,40 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        nums=[]
-        temp=head
+        # nums=[]
+        # temp=head
+        # while temp:
+        #     nums.append(temp.val)
+        #     temp=temp.next
+        # n=len(nums)
+        # nums[k-1],nums[k*-1]=nums[k*-1],nums[k-1]
+        # dummy=ListNode(0)
+        # tail=dummy
+        # for val in nums:
+        #     tail.next=ListNode(val)
+        #     tail=tail.next
+        # return dummy.next
+
+        #Better O(1) space
+        first = last = head
+        temp = head
+        n = 0
+        
         while temp:
-            nums.append(temp.val)
-            temp=temp.next
-        n=len(nums)
-        nums[k-1],nums[k*-1]=nums[k*-1],nums[k-1]
-        dummy=ListNode(0)
-        tail=dummy
-        for val in nums:
-            tail.next=ListNode(val)
-            tail=tail.next
-        return dummy.next
+            n += 1
+            temp = temp.next
+        
+        # Move first to the kth node from start
+        for i in range(k - 1):
+            first = first.next
+        
+        # Move last to the kth node from end
+        last = head
+        for i in range(n - k):
+            last = last.next
+        
+        # Swap values
+        first.val, last.val = last.val, first.val
+        return head
 
         
