211396b67ba789d63df876001a75cc779e0cf0f2
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-08-28 18:45:12 +0530

    Time: 52 ms (96.95%) | Memory: 18.1 MB (25.73%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,24 +1,45 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res=""
-        maxLen=0
-        for i in range(len(s)):
-            # for odd
-            l,r=i,i
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1) > maxLen:
-                    res=s[l:r+1]
-                    maxLen=r-l+1
-                l-=1
-                r+=1
+        # O(n^2) time and O(1) or O(n) space
+        # res=""
+        # maxLen=0
+        # for i in range(len(s)):
+        #     # for odd
+        #     l,r=i,i
+        #     while l>=0 and r<len(s) and s[l]==s[r]:
+        #         if (r-l+1) > maxLen:
+        #             res=s[l:r+1]
+        #             maxLen=r-l+1
+        #         l-=1
+        #         r+=1
 
-            #for even
-            l,r=i,i+1
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1) > maxLen:
-                    res=s[l:r+1]
-                    maxLen=r-l+1
-                l-=1
-                r+=1    
-        return res
+        #     #for even
+        #     l,r=i,i+1
+        #     while l>=0 and r<len(s) and s[l]==s[r]:
+        #         if (r-l+1) > maxLen:
+        #             res=s[l:r+1]
+        #             maxLen=r-l+1
+        #         l-=1
+        #         r+=1    
+        # return res
+
+        # Most Optimal O(n) time and O(n) space which I also dont know how this is working-->Manacher's Algorithm
+        def manacher(s):
+            t = '#' + '#'.join(s) + '#'
+            n = len(t)
+            p = [0] * n
+            l, r = 0, 0
+            for i in range(n):
+                p[i] = min(r - i, p[l + (r - i)]) if i < r else 0
+                while (i + p[i] + 1 < n and i - p[i] - 1 >= 0
+                       and t[i + p[i] + 1] == t[i - p[i] - 1]):
+                    p[i] += 1
+                if i + p[i] > r:
+                    l, r = i - p[i], i + p[i]
+            return p
+
+        p = manacher(s)
+        resLen, center_idx = max((v, i) for i, v in enumerate(p))
+        resIdx = (center_idx - resLen) // 2
+        return s[resIdx : resIdx + resLen]
         
