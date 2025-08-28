7f72efd8e81c0547fba084e4b26df3d9647977c5
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-08-28 17:31:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (6.00%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,14 +1,34 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROWS,COLS=len(matrix),len(matrix[0])
-        i=0
-        j=ROWS*COLS-1
-        while i<=j:
-            mid=(i+j)//2
-            if matrix[mid//COLS][mid%COLS]==target:
+        # Most Optimal O(log(m*n))
+        # ROWS,COLS=len(matrix),len(matrix[0])
+        # i=0
+        # j=ROWS*COLS-1
+        # while i<=j:
+        #     mid=(i+j)//2
+        #     if matrix[mid//COLS][mid%COLS]==target:
+        #         return True
+        #     elif matrix[mid//COLS][mid%COLS]<target:
+        #         i=mid+1
+        #     else:
+        #         j=mid-1
+        # return False
+
+        # Same code for better understanding
+        n = len(matrix)
+        m = len(matrix[0])
+
+        # apply binary search:
+        low = 0
+        high = n * m - 1
+        while low <= high:
+            mid = (low + high) // 2
+            row = mid // m
+            col = mid % m
+            if matrix[row][col] == target:
                 return True
-            elif matrix[mid//COLS][mid%COLS]<target:
-                i=mid+1
+            elif matrix[row][col] < target:
+                low = mid + 1
             else:
-                j=mid-1
+                high = mid - 1
         return False
