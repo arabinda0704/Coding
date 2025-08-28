249f3e86bbd0c558b5d13a743b05c62b33eb2b1c
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-08-28 13:09:33 +0530

    Time: 3 ms (71.07%) | Memory: 18.4 MB (54.47%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -24,10 +24,11 @@ class Solution:
             for j in range(COLS):
                 if matrix[i][j]==0:
                     matrix[0][j]=0
-                    if i>0:
-                        matrix[i][0]=0
-                    else:
+                    if i==0:
                         rowZero=True
+                    else:
+                        matrix[i][0]=0
+                        
         for i in range(1,ROWS):
             for j in range(1,COLS):
                 if matrix[0][j]==0 or matrix[i][0]==0:
