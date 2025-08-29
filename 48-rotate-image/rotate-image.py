249473cf89e0600ab4ac1847777f2755da222a67
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-08-29 17:08:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (41.30%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,17 +4,26 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         #Since it is a square matrix
-        l,r=0,len(mat)-1
-        while l<r:
-            for i in range(r-l):
-                top,bottom=l,r
-                topLeft=mat[top][l+i]
+        # l,r=0,len(mat)-1
+        # while l<r:
+        #     for i in range(r-l):
+        #         top,bottom=l,r
+        #         topLeft=mat[top][l+i]
 
-                mat[top][l+i]=mat[bottom-i][l]
-                mat[bottom-i][l]=mat[bottom][r-i]
-                mat[bottom][r-i]=mat[top+i][r]
-                mat[top+i][r]=topLeft
+        #         mat[top][l+i]=mat[bottom-i][l]
+        #         mat[bottom-i][l]=mat[bottom][r-i]
+        #         mat[bottom][r-i]=mat[top+i][r]
+        #         mat[top+i][r]=topLeft
 
-            l+=1
-            r-=1
+        #     l+=1
+        #     r-=1
         
+        #Another by Transposing
+        n = len(mat)
+        # transposing the matrix
+        for i in range(n-1):
+            for j in range(i+1,n):
+                mat[i][j], mat[j][i] = mat[j][i],mat[i][j]
+        # reversing each row of the matrix
+        for i in range(n):
+            mat[i].reverse()
