57d3bf60612f457037fecf1a56994ccd9e372fd4
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-09-02 23:01:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (63.93%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -1,35 +1,25 @@
 class MyQueue:
 
     def __init__(self):
-        self.s1=[]
-        self.s2=[]
-
+        self.s1,self.s2=[],[]
         
-
     def push(self, x: int) -> None:
         self.s1.append(x)
+        
 
     def pop(self) -> int:
         if not self.s2:
             while self.s1:
                 self.s2.append(self.s1.pop())
         return self.s2.pop()
-        
 
-    # def peek(self) -> int:
-    #     if not self.s2:
-    #         while self.s1:
-    #             self.s2.append(self.s1.pop())
-    #     return self.s2[-1]
     def peek(self) -> int:
         if not self.s2:
             return self.s1[0]
         return self.s2[-1]
-        
 
     def empty(self) -> bool:
-        return len(self.s1) == 0 and len(self.s2)==0
-        # return max(len(self.s1),len(self.s2))==0
+        return len(self.s1)==0 and len(self.s2)==0
         
 
 
