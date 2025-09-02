34d4d2576c7790d01d4da70fd0db96866bf57583
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-09-02 17:19:53 +0530

    Time: 3 ms (23.81%) | Memory: 17.9 MB (32.68%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,51 +1,15 @@
 class Solution:
-    # def __init__(self):
-    #     self.q=deque()
-
-        
-
-    # def Push(self, x: int) -> None:
-    #     # self.push(x)
-    #     self.q.append(x)
-
-        
-
-    # def Pop(self) -> int:
-    #     for i in range(len(self.q)-1):
-    #         self.q.append(self.q.popleft())
-    #     return self.q.popleft()
-
-    # def isValid(self, s: str) -> bool:
-    #     d=Solution()
-    #     for c in s:
-    #         if c in "({[": #c == '(' or c == '{' or c == '[': 
-    #             d.Push(c)
-    #         else:
-    #             if len(d.q) == 0:
-    #                 return False
-
-    #             ch = d.Pop()
-    #             if (c == ')' and ch == '(') or (c == ']' and ch == '[') or (c == '}' and ch == '{'):
-    #                 continue
-    #             else:
-    #                 return False
-    
-    #     return len(d.q) == 0
     def isValid(self, s: str) -> bool:
         d=[]
         for c in s:
-            if c in "({[": #c == '(' or c == '{' or c == '[': 
+            if c in "({[":
                 d.append(c)
             else:
-                if len(d) == 0:
+                if len(d)==0:
                     return False
-
-                ch = d.pop()
-                if (c == ')' and ch == '(') or (c == ']' and ch == '[') or (c == '}' and ch == '{'):
+                ch=d.pop()
+                if (ch=="(" and c==")") or (ch=="{" and c=="}") or (ch=="[" and c=="]"):
                     continue
                 else:
                     return False
-    
-        return len(d) == 0
-
-        
+        return len(d)==0
