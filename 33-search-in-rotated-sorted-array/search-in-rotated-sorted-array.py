f7e28ca55f726a43a1866bbea22fc9a30cfda49b
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-09-03 16:16:11 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (22.51%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -6,17 +6,18 @@ class Solution:
             if nums[mid]==target:
                 return mid
             #Left Sorted
-            elif nums[mid]>=nums[l]:
-                if nums[l]<=target<=nums[mid]: 
+            if nums[l]<=nums[mid]:
+                if nums[l]<=target<=nums[mid]:
                     r=mid-1
                 else:
                     l=mid+1
     
             #Right Sorted
             else:
-                if nums[mid] <= target <= nums[r]:
-                    l = mid + 1
+                if nums[mid]<=target<=nums[r]:
+                    l=mid+1
                 else:
-                    r = mid - 1
+                    r=mid-1
         return -1
+            
         
