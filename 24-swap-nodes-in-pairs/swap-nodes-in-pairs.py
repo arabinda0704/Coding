209c9adb34836be5d9872c48070bcb551f9e768d
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-09-04 00:58:28 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (11.48%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -21,28 +21,38 @@ class Solution:
             curr=curr.next if curr else None
         return curr
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        k=2
-        if not head or not head.next:
-            return head
-        temp=head
+        # k=2
+        # if not head or not head.next:
+        #     return head
+        # temp=head
 
-        prevnode=None
-        while temp:
-            kthNode=self.getKthNode(temp,k)
-            if not kthNode:
-                if prevNode:
-                    prevNode.next=nxt#or temp
-                break
-            nxt=kthNode.next
-            kthNode.next=None
-            self.reverse(temp)
-            if temp==head:
-                head=kthNode
-            else:
-                prevNode.next=kthNode
-            prevNode=temp
-            temp=nxt
-        return head
+        # prevnode=None
+        # while temp:
+        #     kthNode=self.getKthNode(temp,k)
+        #     # if not kthNode:
+        #     #     if prevNode:
+        #     #         prevNode.next=nxt#or temp
+        #     #     break
+        #     #Below line also works instead of above since last node reverse or not does not matter
+        #     if not kthNode:
+        #         last=temp
+        #         while last and last.next:
+        #             last=last.next
+        #         self.reverse(temp)
+        #         if prevNode:
+        #             prevNode.next=last
+        #         break
+
+        #     nxt=kthNode.next
+        #     kthNode.next=None
+        #     self.reverse(temp)
+        #     if temp==head:
+        #         head=kthNode
+        #     else:
+        #         prevNode.next=kthNode
+        #     prevNode=temp
+        #     temp=nxt
+        # return head
 
         #Using recursion
         # Base case
@@ -57,3 +67,24 @@ class Solution:
 
         # # Return new head
         # return second
+
+        #Practice
+        if not head or not head.next:
+            return head
+        dummy=ListNode(0)
+        prev=dummy
+        while head and head.next:
+            first=head
+            second=head.next
+
+            #Swapping
+            prev.next=second
+            first.next=second.next
+            second.next=first
+
+            prev=second.next
+            head=first.next
+        return dummy.next
+
+
+
