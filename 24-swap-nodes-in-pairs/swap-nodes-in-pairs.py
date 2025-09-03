dc6e7269ab2a7b8b6f71ae7e082dfb1796bf4415
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-09-03 20:16:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (53.30%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -4,38 +4,56 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # if not head or not head.next:
-        #     return head
-        # first,second,prev=head,head.next,None
-        # while first and second:
-        #     third=second.next
+    def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev=None
+        curr=head
+        while curr:
+            nxt=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nxt
+        return prev
 
-        #     second.next=first
-        #     first.next=third
-        #     if prev!=None:
-        #         prev.next=second
-        #     else:
-        #         head=second
-        #     #Updation
-        #     prev=first
-        #     first=third
-        #     if third!=None:
-        #         second=third.next
-        #     else:
-        #         second=None
-        # return head
+    def getKthNode(self,head,k):
+        curr=head
+        while k-1 and curr:
+            k-=1
+            curr=curr.next if curr else None
+        return curr
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        k=2
+        if not head or not head.next:
+            return head
+        temp=head
+
+        prevnode=None
+        while temp:
+            kthNode=self.getKthNode(temp,k)
+            if not kthNode:
+                if prevNode:
+                    prevNode.next=nxt#or temp
+                break
+            nxt=kthNode.next
+            kthNode.next=None
+            self.reverse(temp)
+            if temp==head:
+                head=kthNode
+            else:
+                prevNode.next=kthNode
+            prevNode=temp
+            temp=nxt
+        return head
 
         #Using recursion
         # Base case
-        if not head or not head.next:
-            return head
-        first = head
-        second = head.next
-        first.next = self.swapPairs(second.next)
+        # if not head or not head.next:
+        #     return head
+        # first = head
+        # second = head.next
+        # first.next = self.swapPairs(second.next)
 
-        # Swap
-        second.next = first
+        # # Swap
+        # second.next = first
 
-        # Return new head
-        return second
+        # # Return new head
+        # return second
