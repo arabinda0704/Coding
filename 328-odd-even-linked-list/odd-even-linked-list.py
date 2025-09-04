ca201cf9dbdadb827874e04c009da8d6a541d4d8
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-09-04 17:47:30 +0530

    Time: 3 ms (16.24%) | Memory: 19.4 MB (29.79%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -37,9 +37,9 @@ class Solution:
         while even and even.next:
             odd.next=odd.next.next
             even.next=even.next.next
-
             odd=odd.next
             even=even.next
+
         odd.next=evenHead
         return head
 
