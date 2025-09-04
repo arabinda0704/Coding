27503094c94ddce2d97aac9b4bed3dcfd2d5ebbc
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-09-04 17:32:25 +0530

    Time: 81 ms (77.04%) | Memory: 27.8 MB (69.33%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -20,13 +20,29 @@ class Solution:
         # return t1 #or t2
 
         #Same code shorter version
-        t1,t2=headA,headB
-        while t1!=t2:
-            t1=t1.next if t1 else headB
-            t2=t2.next if t2 else headA
-            if t1==t2:
-                return t1 #or t2
-        return t1 #or t2
+        # t1,t2=headA,headB
+        # while t1!=t2:
+        #     t1=t1.next if t1 else headB
+        #     t2=t2.next if t2 else headA
+        #     if t1==t2:
+        #         return t1 #or t2
+        # return t1 #or t2
         
+
+        #Practice
+        t1=headA
+        t2=headB
+        while t1 !=t2:
+            if t1:
+                t1=t1.next
+            else:
+                t1=headB
+            if t2:
+                t2=t2.next
+            else:
+                t2=headA
+            if t1==t2:
+                return t2
+        return t2
     
         
