f49501317cfefd82b62f904b943c436e2d403ae6
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-09-08 21:51:51 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (16.58%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,17 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        self.res = []
-        self.backtrack([], nums, [False] * len(nums))
-        return self.res
+        res = []
+        self.backtrack(res,[], nums, [False] * len(nums))
+        return res
 
-    def backtrack(self, perm: List[int], nums: List[int], pick: List[bool]):
+    def backtrack(self,res : List[List[int]], perm: List[int], nums: List[int], pick: List[bool]):
         if len(perm) == len(nums):
-            self.res.append(perm[:])
+            res.append(perm[:])
             return
         for i in range(len(nums)):
             if not pick[i]:
                 perm.append(nums[i])
                 pick[i] = True
-                self.backtrack(perm, nums, pick)
+                self.backtrack(res,perm, nums, pick)
                 perm.pop()
                 pick[i] = False
