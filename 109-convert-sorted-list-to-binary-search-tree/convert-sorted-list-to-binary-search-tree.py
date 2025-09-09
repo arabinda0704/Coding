666d0e0b39a458e0260e38ee772c249f22b52596
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-09-09 20:50:21 +0530

    Time: 5 ms (82.47%) | Memory: 20.5 MB (71.34%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
@@ -11,25 +11,49 @@
 #         self.right = right
 class Solution:
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        arr=[]
-        curr=head
-        while curr:
-            arr.append(curr.val)
-            curr=curr.next
-        def sortedArrayToBST(nums):
-            if not nums:
+        # arr=[]
+        # curr=head
+        # while curr:
+        #     arr.append(curr.val)
+        #     curr=curr.next
+        # def sortedArrayToBST(nums):
+        #     if not nums:
+        #         return None
+
+        #     # Pick the middle element as the root
+        #     mid = len(nums) // 2
+        #     root = TreeNode(nums[mid])
+
+        #     # Recursively build left and right subtrees
+        #     root.left = sortedArrayToBST(nums[:mid])
+        #     root.right = sortedArrayToBST(nums[mid+1:])
+
+        #     return root
+        # return sortedArrayToBST(arr)
+
+        #Without Extra Space Better
+        def listToTree(head):
+            if not head:
                 return None
+            if not head.next:
+                return TreeNode(head.val)
 
             # Pick the middle element as the root
-            mid = len(nums) // 2
-            root = TreeNode(nums[mid])
+            slow,fast=head,head
+            prev=None
+            while fast and fast.next:
+                prev=slow
+                slow=slow.next
+                fast=fast.next.next
+            prev.next=None
+            root = TreeNode(slow.val)
 
             # Recursively build left and right subtrees
-            root.left = sortedArrayToBST(nums[:mid])
-            root.right = sortedArrayToBST(nums[mid+1:])
+            root.left = listToTree(head)
+            root.right = listToTree(slow.next)
 
             return root
-        return sortedArrayToBST(arr)
-        
+        return listToTree(head)
+            
 
         
