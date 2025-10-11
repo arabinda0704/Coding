7a67649b4b3313c3bd249da9e2437f90b689f93e
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-10-11 15:28:38 +0530

    Time: 27 ms (34.05%) | Memory: 17.8 MB (84.13%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,28 +1,27 @@
 from typing import List
 
 class Solution:
+    def dfs(self, i, cur, target, res, nums):
+        if target == 0:
+            res.append(cur[:])
+            return
+        
+        if i == len(nums) or target < 0:
+            return
+        
+        # include current number
+        cur.append(nums[i])
+        self.dfs(i + 1, cur, target - nums[i], res, nums)
+        cur.pop()
+
+        # skip all duplicates of nums[i]
+        while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+            i += 1
+        
+        # exclude current number
+        self.dfs(i + 1, cur, target, res, nums)
     def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
         res = []
-        nums.sort()   # sort once to handle duplicates
-
-        def dfs(i, cur, target):
-            if target == 0:
-                res.append(cur[:])   # found a valid combination
-                return
-            if target < 0:
-                return
-
-            for j in range(i, len(nums)):
-                # \U0001f511 skip duplicates
-                if j > i and nums[j] == nums[j-1]:
-                    continue
-                # prune if number exceeds target
-                if nums[j] > target:
-                    break
-
-                cur.append(nums[j])
-                dfs(j+1, cur, target - nums[j])  # move forward
-                cur.pop()
-
-        dfs(0, [], target)
+        self.dfs(0, [],target,res,nums)
         return res
