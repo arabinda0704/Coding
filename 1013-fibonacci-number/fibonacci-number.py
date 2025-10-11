3986e3ea8fe45cbfb5543b39969ee4bfaca9666a
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-10-11 20:57:52 +0530

    Time: 36 ms (77.19%) | Memory: 17.8 MB (23.64%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -2,7 +2,7 @@ class Solution:
     def fib(self, n: int) -> int:
         if n <= 1:
             return n
-        dp = [0] * (n + 1)
+        dp = [0] * (n+1)#n+1 because starts from 0th fibo or 0th ind
         dp[1] = 1
         for i in range(2, n + 1):
             dp[i] = dp[i-1] + dp[i-2]
