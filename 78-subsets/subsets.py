cb9f691f29a65b1e0f24fdf473d7e1299e48f4d9
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-11-04 00:56:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (90.43%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -9,8 +9,26 @@ class Solution:
         self.dfs(i + 1,nums,subset,res)
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        # res = []
+        # subset = []
+        # self.dfs(0,nums,subset,res)
+        # return res
         res = []
-        subset = []
-        self.dfs(0,nums,subset,res)
+        # nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return
+
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+
+            # while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+            #     i += 1
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
         return res
 
