1cd732c1c0a2881db4f14f976587466f55c04171
Arabinda Das <123241215+arabinda0704@users.noreply.github.com>
2025-11-05 01:13:21 +0530

    Time: 7 ms (96.75%) | Memory: 18.2 MB (83.05%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,29 +1,62 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        ans=[]
-        rows=set()
-        negDiag=set() #r-c
-        posDiag=set() #r+c
+        # ans=[]
+        # rows=set()
+        # negDiag=set() #r-c
+        # posDiag=set() #r+c
 
-        board=[["."]*n for _ in range(n)]
+        # board=[["."]*n for _ in range(n)]
 
-        def backtrack(c):
-            if c==n:
-                copy=["".join(col) for col in board]
-                ans.append(copy)
-            for r in range(n):
-                if r in rows or r+c in posDiag or r-c in negDiag:
+        # def backtrack(c):
+        #     if c==n:
+        #         copy=["".join(col) for col in board]
+        #         ans.append(copy)
+        #     for r in range(n):
+        #         if r in rows or r+c in posDiag or r-c in negDiag:
+        #             continue
+        #         rows.add(r)
+        #         posDiag.add(r+c)
+        #         negDiag.add(r-c)
+        #         board[r][c]="Q"
+
+        #         backtrack(c+1)
+
+        #         rows.remove(r)
+        #         posDiag.remove(r+c)
+        #         negDiag.remove(r-c)
+        #         board[r][c]="."
+        # backtrack(0)
+        # return ans
+
+        # Same in a different way
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        res = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
                     continue
-                rows.add(r)
-                posDiag.add(r+c)
-                negDiag.add(r-c)
-                board[r][c]="Q"
 
-                backtrack(c+1)
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
 
-                rows.remove(r)
-                posDiag.remove(r+c)
-                negDiag.remove(r-c)
-                board[r][c]="."
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+
         backtrack(0)
-        return ans
+        return res
